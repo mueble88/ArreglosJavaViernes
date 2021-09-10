01bb3b84158a4bdbe026c0d70e2e2c9d4ced37a1
juan camilo jaramillo arboleda <camilo88mueble@gmail.com>
2021-09-10 10:28:24 -0500

    se crea arreglo

--- src/arreglos/Arreglos.java
+++ src/arreglos/Arreglos.java
@@ -3,16 +3,32 @@
  */
 package arreglos;
 
+import java.util.Scanner;
 import paqueteDos.Heroe;
 
 public class Arreglos {
 
     public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner (System.in);
+        int[] numeros = new int[4];
+        
+        
+        for (int i = 0; i < numeros.length; i++) {
+            
+        }
+        
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+        
+        
+        
         // Como se utiliza una clase?
         //que es un objeto ?
         //es una variable
         
-        Heroe heroe=new Heroe();
+        /*Heroe heroe=new Heroe();
         Enemigo enemigo= new Enemigo();
         
         //accediendo a un atributo privado
@@ -22,7 +38,7 @@ public class Arreglos {
         enemigo.nombre="Borré";
         heroe.nombre="Lucho Diaz";
         
-        System.out.println(heroe.getEdad());
+        System.out.println(heroe.getEdad());*/
         
     }
     
